0b90be32c4fcaa3b73d6224ca0c3ce6e3eaa6880
alex <elkikos@gmail.com>
2022-07-28 22:19:57 +0200

    g

--- csvlog.py
+++ csvlog.py
@@ -11,7 +11,7 @@ Max_File_Size = 50000
 
 
 
-def WriteLine(passed):
+def WriteDataLine(passed):
     log = open(LogFileName,"a") #open in append - creates if not existing, will append if it exists
     log.write(passed)
     log.close()
@@ -21,4 +21,4 @@ def WriteSensorLog(timeStamp:DateTimeObj, sensorData:SensorData):
     print("logging...")
     timeString = f"{timeStamp.year}{timeStamp.month:02d}{timeStamp.day:02d} {timeStamp.hour:02d}:{timeStamp.minute:02d}:{timeStamp.second:02d}"
     csvString = f"\"{timeString}\",\"{sensorData.dht22.Temperature}\",\"{sensorData.dht22.Humidity}\"\n"
-    WriteLine(csvString)
+    WriteDataLine(csvString)
